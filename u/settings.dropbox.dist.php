9831d44eac408c20eb4da6aa639d3f21144b47c8
goorus <goorus@list.ru>
2014-01-29 23:21:05 +0400

    Dropbox-sdk package added

---
+++ u/settings.dropbox.dist.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * You will need to log in to your Dropbox account and create a new app.
+ * https://www.dropbox.com/developers/apps/create
+ * $key is a 32-byte encryption key (secret)
+ */
+return array(
+    'admin' => array('XXXXXXXX', 'YYYYYYY'),
+    'user_name' => array('application_key', 'secret'),
+);
